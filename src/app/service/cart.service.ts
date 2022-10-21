161d0705465c3e81625ae4296b8f207ce5a7e887
Arbaz khan <arbaz582021@gmail.com>
2022-10-21 14:44:24 +0500

    add

--- src/app/service/cart.service.ts
+++ src/app/service/cart.service.ts
@@ -8,7 +8,7 @@ import { foods } from '../shared/modal/food';
 })
 export class CartService {
   private cart:cart=new cart();
-  // cartItem: any; //error occur due to the line no 19 cartItem if you feel stuck in future due to this cartItem then reomove cartItem:any and this.cartItem from line no 19
+  cartItem: any; //error occur due to the line no 19 cartItem if you feel stuck in future due to this cartItem then reomove cartItem:any and this.cartItem from line no 19
  
   addToCart(food:foods):void{
     let cartItem=this.cart.items.find(item=>item.food.id===food.id)
@@ -16,7 +16,7 @@ export class CartService {
       this.changeQuantity(food.id,cartItem.quantity+1);
       return;
     }
-    // this.cart.items.push(new this.cartItem(food));
+    this.cart.items.push(new this.cartItem(food));
   }
   removeFromCart(foodId:number):void{
     this.cart.items=this.cart.items.filter(item=>item.food.id !=foodId)
